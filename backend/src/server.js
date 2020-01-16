97ead47ea00c943c3ae1d82f4a6e7818abf8f998
LucDSilva <lucas.silva00@outlook.ie>
2020-01-16 12:29:07 +0000

    added CORS to express server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => { console.log('MongoDB Connected')});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
